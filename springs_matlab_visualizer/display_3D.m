5daba69943dbe53f9b51b3e79d2641981b6715f8
Jake <jake@Jacobs-Air-5.fios-router.home>
2020-08-07 13:43:48 -0400

    example 3D
    example 3D spring network, similar to traction force example in 2D.  updated 3D visualization for matlab too.

--- springs_matlab_visualizer/display_3D.m
+++ springs_matlab_visualizer/display_3D.m
@@ -1,34 +1,139 @@
 function h = display_3D( nodes , springs , varargin )
+% display_3D( nodes , springs ) plots all springs as black edges.
+% 
+% h = display_3D( nodes , springs ) returns handles to the new figure and axes.
+%
+% display_3D( nodes , springs , color_variable , color_range ) plots all springs with color
+% determined according to color_variable linearly within min/max of color_range.  Color_variable
+% may be either a string corresponding to a field of springs data structure, or it may be an array
+% of size NS x 1 where NS is the number of springs. Default color_range is [0,1].  Default variable
+% is none.
+%
+% display_3D( nodes , springs , color_variable , color_range , color_map ) plots all springs
+% with the number of colors NC, and RGB values of each color, specified as an NC x 3 array
+% color_map. Default is jet(10)
+%
+% display_3D( nodes , springs , color_variable , color_range , color_map , show_forces ) also plots
+% the external applied force at each node with black arrows, scaled by the largest force magnitude,
+% if show_forces is true.  Default is false.
+%
+% Example:
+% display_3D( nodes , springs , 'stiffness_tension' , [0,1] , jet(11) , true )
+%
 
+
+% default parameters
+color_variable = '' ;
+color_range = [0,1] ;
+color_map = jet(10) ;
+show_forces = false ;
+
+% user-defined parameters
+if numel(varargin) > 0
+	color_variable = varargin{1} ;
+end
+if numel(varargin) > 1
+	color_range = varargin{2} ;
+	color_range = sort( color_range ) ;
+end
+if numel(varargin) > 2
+	color_map = varargin{3} ;
+end
+if numel(varargin) > 3
+	show_forces = varargin{4} ;
+end
+
+% group springs according to color_variable
+% or make all springs black by default
+if isempty( color_variable )
+	color_map = [0,0,0] ;
+	color_ind = ones( [ size(springs.nodes,1) , 1 ] ) ;
+elseif ischar( color_variable )
+	if ~isfield( springs , color_variable )
+		fprintf( 'ERROR: %s is not a property of springs\n' , color_variable )
+		color_map = [0,0,0] ;
+		color_ind = ones( [ size(springs.nodes,1) , 1 ] ) ;
+	else
+		color_val = springs.(color_variable)(:,1) ;
+		color_ind = (color_val-min(color_range))/range(color_range) ;
+		color_ind = min(1, max(0, color_ind )) ;
+		color_ind = 1 + round( (size(color_map,1)-1) * color_ind ) ;
+	end
+else
+	if ~isequal( size(color_variable,1) , size(springs.nodes,1) )
+		color_map = [0,0,0] ;
+		color_ind = ones( [ size(springs.nodes,1) , 1 ] ) ;
+	else
+		color_val = color_variable ;
+		color_ind = (color_val-min(color_range))/range(color_range) ;
+		color_ind = min(1, max(0, color_ind )) ;
+		color_ind = 1 + round( (size(color_map,1)-1) * color_ind ) ;
+	end
+end
+
+%
 h.fig = figure( ...
-	'Position' , [0,0,1400,700] , ...
+	'Colormap' , color_map , ...
 	'Color' , [1,1,1] ) ;
 h.ax = axes( ...
-	'XLim' , [ -1 34 ] , ...
-	'YLim' , [ -1 34 ] , ...
-	'ZLim' , [ -1 34 ] , ...
-	'View' , [0,90],...%[ -25 , 25 ] , ...%
+	'XLim' , [ -1 10 ] , ...
+	'YLim' , [ -1 10 ] , ...
+	'ZLim' , [ -1 10 ] , ...
+	'CLim' , color_range , ...
+	'View' , [ -25 , 25 ] , ...
 	'DataAspectRatio' , [1,1,1] , ...
+	'Clipping' , 'off' , ...
+	'TickDir' , 'out' , ...
 	'NextPlot' , 'add' ) ;
-xx = [ nodes.position( springs.nodes(:,1) ,1) , nodes.position( springs.nodes(:,2) ,1) ] ;
-yy = [ nodes.position( springs.nodes(:,1) ,2) , nodes.position( springs.nodes(:,2) ,2) ] ;
-zz = [ nodes.position( springs.nodes(:,1) ,3) , nodes.position( springs.nodes(:,2) ,3) ] ;
-xx(:,3) = nan ;
-yy(:,3) = nan ;
-zz(:,3) = nan ;
-plot3( ...
-	reshape( xx' ,[],1) , ...
-	reshape( yy' ,[],1) , ...
-	reshape( zz' ,[],1) , ...
-	'k-' , ...
-	'LineWidth' , 1 )
-for pp = 1 : size(nodes.position,1)
+
+for ii = 1 : size(color_map,1)
+	ind = find( color_ind == ii ) ;
+	if ~isempty( ind )
+		xx = [ nodes.position( springs.nodes(ind,1) ,1) , nodes.position( springs.nodes(ind,2) ,1) ] ;
+		yy = [ nodes.position( springs.nodes(ind,1) ,2) , nodes.position( springs.nodes(ind,2) ,2) ] ;
+		zz = [ nodes.position( springs.nodes(ind,1) ,3) , nodes.position( springs.nodes(ind,2) ,3) ] ;
+		xx(:,3) = nan ;
+		yy(:,3) = nan ;
+		zz(:,3) = nan ;
+		plot3( ...
+			reshape( xx' ,[],1) , ...
+			reshape( yy' ,[],1) , ...
+			reshape( zz' ,[],1) , ...
+			'-' , ...
+			'Color' , color_map(ii,:) , ...
+			'LineWidth' , 1 ) ;
+	end
+end
+
+axis tight
+axis_size = max( [ range(xlim()) , range(ylim()) , range(zlim()) ] ) ;
+
+if show_forces
+	force_mag_sq = sum(power( nodes.force ,2),2) ;
+	ind = (force_mag_sq>0) & (force_mag_sq>(1e-3*max(force_mag_sq))) ;
+	force_scaled = nodes.force(ind,:) / sqrt(max(force_mag_sq)) ;
+	
+	force_scaled = force_scaled ;
+	
+	xx = bsxfun(@plus, nodes.position(ind,1) , bsxfun(@times, force_scaled(:,1) , [0,1] ) ) ;
+	yy = bsxfun(@plus, nodes.position(ind,2) , bsxfun(@times, force_scaled(:,2) , [0,1] ) ) ;
+	zz = bsxfun(@plus, nodes.position(ind,3) , bsxfun(@times, force_scaled(:,3) , [0,1] ) ) ;
+	xx(:,end+1) = nan ;
+	yy(:,end+1) = nan ;
+	zz(:,end+1) = nan ;
+	
 	plot3( ...
-		nodes.position(pp,1) + [0,nodes.force(pp,1)] , ...
-		nodes.position(pp,2) + [0,nodes.force(pp,2)] , ...
-		nodes.position(pp,3) + [0,nodes.force(pp,3)] , ...
-		'r-' , ...
-		'LineWidth' , 1 )
+		reshape( xx' ,[],1) , ...
+		reshape( yy' ,[],1) , ...
+		reshape( zz' ,[],1) , ...
+		'k-' , ...
+		'LineWidth' , 3 )
+end
+
+axis tight
+
+if nargout > 0
+	varargout{1} = h ;
 end
 
 end
---
+++ springs_sample_tractionForce3D.m
@@ -0,0 +1,163 @@
+%% DESCRIPTION
+
+% this example shows a cubic grid in 3D, with all fixed
+% boundaries, and external forces applied at random locations.
+% spring compression is allowed, and all springs are initially
+% at their rest lengths. best solver algorithm/objective seems
+% to be anneal/maxforce for this scenario.
+
+%%
+
+addpath( 'springs_matlab_solver' )
+addpath( 'springs_matlab_visualizer' )
+
+%% parameters for rectangular grid
+
+num_row = 7 ;
+num_col = 8 ;
+num_lay = 9 ;
+k_mean = 1.0 ;
+k_stdv = 0.0 ;
+L0_mean = 1.0 ;
+L0_stdv = 0.0 ;
+
+%% PREPROCESS
+
+% row, column, layer index of each node in the rectangular grid
+[ col , row , lay ] = meshgrid( 1:num_col , 1:num_row , 1:num_lay ) ;
+
+% location of each node, each row of this array corresponds
+% to one nodes's row index and column index
+loc = cat(2, ...
+	reshape( row ,[],1) , ...
+	reshape( col ,[],1) , ...
+	reshape( lay ,[],1) ) ;
+
+% adjacent nodes are identified from each node looking backwards
+% to the row-1, col-1, lay-1 indexes, forming a 3D lattice with
+% 6-point connectivity.
+adj = [
+	bsxfun(@plus, loc , [ 0,-1, 0] )
+	bsxfun(@plus, loc , [-1, 0, 0] )
+	bsxfun(@plus, loc , [ 0, 0,-1] )
+	] ;
+
+% each spring connects two adjacent nodes.
+% each row of this array correspond to one spring's start and
+% end nodes.
+spring_nodes = cat(2, ...
+	repmat( (1:size(loc,1))' ,[3,1]) , ...
+	adj(:,1) + num_row*(adj(:,2)-1) + num_row*num_col*(adj(:,3)-1) ) ;
+
+% remove springs with nodes that point outside the boundary
+remove = any( bsxfun(@lt,adj,[1,1,1]) | bsxfun(@gt,adj,[num_row,num_col,num_lay]) ,2) ;
+spring_nodes( remove ,:) = [] ;
+
+% randomly sample a normal distribution of spring stiffnesses
+spring_stiffness = normrnd( k_mean , k_stdv , [size(spring_nodes,1),1] ) ;
+
+% randomly sample a normal distribution of spring rest lengths
+spring_rest_length = normrnd( L0_mean , L0_stdv , [size(spring_nodes,1),1] ) ;
+
+% remove springs with zero stiffness.
+remove = spring_stiffness <= 0 ;
+spring_stiffness( remove ) = [] ;
+spring_rest_length( remove ,:) = [] ;
+spring_nodes( remove ,:) = [] ;
+
+% fix nodes around the boundary
+boundary = {
+	find(ismember(loc(:,1),1      ))
+	find(ismember(loc(:,1),num_row))
+	find(ismember(loc(:,2),1      ))
+	find(ismember(loc(:,2),num_col))
+	find(ismember(loc(:,2),1      ))
+	find(ismember(loc(:,2),num_lay))
+	} ;
+boundary{3} = setdiff( boundary{3} , [boundary{1};boundary{2}] ) ;
+boundary{4} = setdiff( boundary{4} , [boundary{1};boundary{2}] ) ;
+boundary{5} = setdiff( boundary{5} , [boundary{1};boundary{2};boundary{3};boundary{4}] ) ;
+boundary{6} = setdiff( boundary{6} , [boundary{1};boundary{2};boundary{3};boundary{4}] ) ;
+
+%% CREATE NODES/SPRINGS DATA STRUCTURES
+
+% overall parameters
+num_dimensions = 3 ;            % number of spatial dimensions
+precision = 'double' ;          % floating point precision: 'single' or 'double'
+num_stiffness_tension = 1 ;     % number of polynomial stiffness coefficients for tension
+num_stiffness_compression = 1 ; % number of polynomial stiffness coefficients for compression
+
+% size of network
+num_points = size(loc,1) ;
+num_springs = size(spring_nodes,1) ;
+
+% these are the only mandatory properties of nodes data structure.
+% the springs_solve() command will ignore any other properties.
+% you may add any additional properties as desired.
+nodes.position = zeros( [ num_points , num_dimensions ] ) ;
+nodes.force    = zeros( [ num_points , num_dimensions ] ) ;
+nodes.fixed    = false( [ num_points , num_dimensions ] ) ;
+
+% assign values computed in previous section
+nodes.position = [ col(:) , row(:) , lay(:) ] ;
+nodes.fixed( boundary{1} ,:) = true ;
+nodes.fixed( boundary{2} ,:) = true ;
+nodes.fixed( boundary{3} ,:) = true ;
+nodes.fixed( boundary{4} ,:) = true ;
+nodes.fixed( boundary{5} ,:) = true ;
+nodes.fixed( boundary{6} ,:) = true ;
+
+% these are the only mandatory properties of springs data structure.
+% the springs_solve() command will ignore any other properties.
+% you may add any additional properties as desired.
+springs.nodes                 = zeros( [ num_springs , 2                         ] ) ;
+springs.rest_length           = zeros( [ num_springs , 1                         ] ) ;
+springs.stiffness_tension     = zeros( [ num_springs , num_stiffness_tension     ] ) ;
+springs.stiffness_compression = zeros( [ num_springs , num_stiffness_compression ] ) ;
+springs.compression           = zeros( [ num_springs , 1                         ] ) ;
+
+% assign values computed in previous section
+springs.nodes = spring_nodes ;
+springs.rest_length(:) = spring_rest_length ;
+springs.stiffness_tension(:,1) = spring_stiffness ;
+springs.stiffness_compression(:,1) = spring_stiffness ;
+springs.compression(:) = true ;
+
+%% SOLVE EQUILIBRIUM (NO FORCES)
+
+% solver parameters not specified are treated as default.
+options = springs_default_options() ;
+options.algorithm = 'anneal' ;
+options.objective = 'maxforce' ;
+options.include_force_fixed_nodes = false ;
+options.num_iter_print = 10000 ;
+options.tolerance_change_objective = 1e-24 ;
+options.tolerance_sum_net_force = 1e-24 ;
+
+% run solver
+[ nodes_eq , ~ ] = springs_solve( nodes , springs , options ) ;
+
+%% ADD FORCES AND RESOLVE EQUILIBRIUM
+
+% select a few random nodes (not on boundary) and apply random forces
+num_rand_force = 20 ;
+magnitude = 0.4 ;
+free_node_ind = find( ~any( nodes_eq.fixed ,2) ) ;
+rand_node_ind = free_node_ind( randperm( numel(free_node_ind) ,num_rand_force) ) ;
+rand_force_ang1 = rand( [num_rand_force,1] ) *(2.0*pi) ;
+rand_force_ang2 = rand( [num_rand_force,1] ) * pi ;
+rand_force_mag = zeros( size(rand_force_ang1) ) + magnitude ;
+nodes_eq.force( rand_node_ind ,:) = bsxfun(@times, ...
+	rand_force_mag , ...
+	[sin(rand_force_ang2).*cos(rand_force_ang1),sin(rand_force_ang2).*sin(rand_force_ang1),cos(rand_force_ang2)] ) ;
+
+% run solver
+[ nodes_eq_force , ~ ] = springs_solve( nodes_eq , springs , options ) ;
+
+%% DISPLAY
+
+h_before = display_3D( nodes_eq       , springs , springs_tension(nodes_eq      ,springs) , [0,0.2] , parula(10) , true ) ;
+h_after  = display_3D( nodes_eq_force , springs , springs_tension(nodes_eq_force,springs) , [0,0.2] , parula(10) , true ) ;
+linkaxes( [h_before.ax,h_after.ax] , 'xy' ) ;
+
+%%
