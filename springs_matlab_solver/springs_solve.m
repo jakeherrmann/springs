51abf6df7f056e04dd6e3c4a3910bbef64d1e426
jakeherrmann <50957363+jakeherrmann@users.noreply.github.com>
2023-06-14 15:16:50 -0500

    2023-06-14 update 2
    updated matlab solver command for windows to address permissions issue when calling C++ executable using system() command, using a BAT file instead.

--- springs_matlab_solver/springs_solve.m
+++ springs_matlab_solver/springs_solve.m
@@ -47,10 +47,28 @@ setenv( 'OMP_WAIT_POLICY' , 'active' ) ;
 setenv( 'OMP_PROC_BIND'   , 'true'   ) ;
 sys_command = sprintf( '%s %s %s --verbose %d --parallel %d' , exe_springs_solver , dir_input , dir_output , verbose , use_parallel ) ;
 if verbose
-	system(sys_command) ;
+	sys_failure = system( sys_command ) ;
 else
-	[~,~] = system(sys_command) ;
+	[ sys_failure , ~ ] = system( sys_command ) ;
 end
+if sys_failure
+	fprintf( 'SYSTEM() ERROR CODE: %d\n' , sys_failure )
+	if ispc()
+		fprintf( 'WIN64: attempting call from .bat file\n' )
+		ml_path = fullfile( matlabroot() , 'bin' , 'win64' ) ;
+		batfile = 'dev_solver.bat' ;
+		fid = fopen(batfile,'w') ;
+		fprintf(fid,'set path=%%path:%s;=%%\n',ml_path) ;
+		fprintf(fid,'%s\n',sys_command) ;
+		fclose( fid ) ;
+		if verbose
+			system( batfile ) ;
+		else
+			[ ~ , ~ ] = system( batfile ) ;
+		end
+	end
+end
+
 [ nodes , springs ] = springs_read( dir_output ) ;
 
 end
